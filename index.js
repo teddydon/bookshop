8b66c774941e52b60d9b16f8630e7be8c82c5c9c
teddydon <chibyki200@gmail.com>
2025-12-18 09:19:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const customer_routes = require('./final_project/auth_users.js').authenticated;
+const genl_routes = require('./final_project/general.js').general;
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(session({
+    secret: "fingerprint_customer",
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// Check if session exists
+    if (req.session.authorization) {
+        // Extract the access token
+        let token = req.session.authorization['accessToken'];
+
+        // Verify JWT access token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user; // attach user data
+                next(); // access token valid allow user request
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
 const PORT =5000;
